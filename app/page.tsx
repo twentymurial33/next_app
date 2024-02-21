ffc77889a51e483da66dc54c0539d73e6c0722c4
Murial Anindo <murial.anindo@lonett.com>
2024-02-21 16:02:07 -0500

    added components

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-import Image from 'next/image'
+import Link from 'next/link';
+import ProductCard from './components/ProductCard';
 
 export default function Home() {
   return (
-    
      <main>
       <h1>Hello World</h1>
-     </main> 
-
-
-
-     
-
-       
-
-       
+      <Link href="/users">Users</Link>
+      <ProductCard/>
+     </main>     
   )
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const UsersPage = () => {
+interface User{
+    id:number;
+    name:string;
+}
+
+const UsersPage = async() => {
+    const res= await fetch('')
+    const users:User[] = await res.json()
   return (
-    <div>Users Page</div> 
+    <>
+    <h1>Users</h1>
+    <ul>
+        {users.map(user=><li key={user.id}>{user.name}</li>)}
+    </ul>
+    </> 
   )
 }
 
